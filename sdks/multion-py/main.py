425e527c697b096a419573ecfa096127ba190c43
Naman Garg <ngarg3@binghamton.edu>
2023-11-01 19:15:02 -0700

    update anon credentials

--- sdks/multion-py/main.py
+++ sdks/multion-py/main.py
@@ -6,10 +6,11 @@ import os
 from anon.client import Client
 
 from playwright.async_api import async_playwright
-API_KEY = "ory_st_dOe7LrMuz8LrLBNkPKkV4edyh2l9tY0f"
-APP_USER_ID = "1c8630c9-937e-4f2a-9ebc-206929b093a6"
-SESSION_DOMAIN = "linkedin"
-SESSION_APP_URL = "https://linkedin.com/"
+
+API_KEY = "ory_st_CVLmTJHwTjt5DsmkT7UHZhWf1LwWDVuU"
+APP_USER_ID = "d7273277-820e-4211-8e6a-686190a63f87"
+SESSION_DOMAIN = "uber"
+SESSION_APP_URL = "https://m.uber.com/"
 
 async def main():
     client = Client(API_KEY)
@@ -17,7 +18,7 @@ async def main():
     # Request a session for a user
     account = anon.Account(APP_USER_ID, SESSION_DOMAIN)
     print("requesting session")
-    # await client.request_session(account)
+    await client.request_session(account)
 
     # Wait for the user to create the session
     while True:
@@ -39,26 +40,26 @@ async def main():
 
         # Take actions as the user
         # Send a message on LinkedIn!
-        recipient = "Div Garg"
-        # current time in hh:mm:ss format
-        time_string = time.strftime('%H:%M:%S')
-        message = "hello from Anon! :)" + " The time is " + time_string
-        print("Sending message '", message, "' to", recipient)
+        # recipient = "Div Garg"
+        # # current time in hh:mm:ss format
+        # time_string = time.strftime('%H:%M:%S')
+        # message = "hello from Anon! :)" + " The time is " + time_string
+        # print("Sending message '", message, "' to", recipient)
 
-        await page.goto("https://linkedin.com/messaging/?")
-        await page.main_frame.wait_for_load_state()
-        await page.get_by_placeholder("Search messages").press_sequentially(recipient)
-        await page.wait_for_timeout(1000)
-        await page.get_by_placeholder("Search messages").press("Enter")
-        await page.wait_for_timeout(1000)
+        # await page.goto("https://linkedin.com/messaging/?")
+        # await page.main_frame.wait_for_load_state()
+        # await page.get_by_placeholder("Search messages").press_sequentially(recipient)
+        # await page.wait_for_timeout(1000)
+        # await page.get_by_placeholder("Search messages").press("Enter")
+        # await page.wait_for_timeout(1000)
 
-        await page.get_by_role("listitem").filter(has_text=recipient, has_not=page.locator("#ember")).first.click()
-        await page.wait_for_timeout(1000)
+        # await page.get_by_role("listitem").filter(has_text=recipient, has_not=page.locator("#ember")).first.click()
+        # await page.wait_for_timeout(1000)
 
-        await page.get_by_label("Write a message").press_sequentially(message)
-        await page.wait_for_timeout(1000)
+        # await page.get_by_label("Write a message").press_sequentially(message)
+        # await page.wait_for_timeout(1000)
         
-        await page.get_by_role("button", name="Send", exact=True).click()
+        # await page.get_by_role("button", name="Send", exact=True).click()
 
         # Optional wait period for actions to complete
         await asyncio.sleep(100000)
